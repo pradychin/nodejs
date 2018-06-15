f5aefe91e2bc818b5646a672f22cf46211ef7960
pradychin <prady.chin@gmail.com>
2018-06-15 09:43:55 -0400

    another date format

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var now = new Date();
 console.log(now);
 var x = dateFormat(now, "dddd, mmmm dS, yyyy, h:MM:ss TT");
 console.log(x);
+
+
+var x = dateFormat(now, "dd mmmm yyyy, h:MM:ss TT");
+console.log(x);
